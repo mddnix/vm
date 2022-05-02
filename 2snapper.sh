9cc203cd013670c729cb435fd37c4dccfe065be2
Madhusudhan D Desai <mddnix@gmail.com>
2022-05-02 23:38:40 +0530

    Update 2snapper.sh

--- 2snapper.sh
+++ 2snapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #BTRFS_MOUNT="$(df -h -t btrfs | grep -w '/' | awk '{print $1}')"
-BTRFS_MOUNT=="$(df -h --output=source / | grep dev)"
+BTRFS_MOUNT="$(df -h --output=source / | grep dev)"
 if [[ -z ${BTRFS_MOUNT} ]] ; then
         exit
 fi
